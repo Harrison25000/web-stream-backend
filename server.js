a3f7a6c76c9cca8ee8e72053f93e4a1d1ba4433c
Oliver Harrison <ofcharrison@gmail.com>
2023-03-20 00:28:14 +0000

    viewer number endpoints added set remove retrieve

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ let broadcaster;
 const port = process.env.PORT || 4000;
 
 const http = require("http");
+const { watch } = require("fs");
 const server = http.createServer(app);
+var watchersCount = 0;
+var broadcastersCount = 0;
 
 const io = require("socket.io")(server, {
     cors: {
@@ -44,6 +47,7 @@ io.sockets.on("connection", socket => {
     socket.on("broadcaster", () => {
         broadcaster = socket.id;
         socket.broadcast.emit("broadcaster");
+        broadcastersCount++;
     });
     socket.on("watcher", () => {
         socket.to(broadcaster).emit("watcher", socket.id);
@@ -62,13 +66,26 @@ io.sockets.on("connection", socket => {
     });
 });
 
-router.get('/getio', (req, res) => {
-    return (io);
+app.get('/getio', (req, res) => {
+    res.send(io);
 });
 
+app.get('/getwatchers', (req, res) => {
+    res.send({ count: watchersCount, status: "ok", message: "retrieved number of watchers" });
+});
+
+app.get('/setwatcher', (req, res) => {
+    watchersCount++;
+    res.send({ count: watchersCount, status: "ok", message: "added watcher" });
+});
+
+app.get('/removewatcher', (req, res) => {
+    watchersCount--;
+    res.send({ count: watchersCount, status: "ok", message: "removed watcher" });
+});
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+// })
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
